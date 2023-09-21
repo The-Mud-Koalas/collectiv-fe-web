3639f920253d1bdd702f034a49523b9ebcec608d
Harry-01 <harry@harry-mok.com>
2023-09-21 10:09:14 +1000

    feat: add functionality to forget password page. Email will be sent to email allowing user to reset passcode

--- components/features/accounts/ForgetPassword/ForgetPassword.tsx
+++ components/features/accounts/ForgetPassword/ForgetPassword.tsx
@@ -2,73 +2,109 @@ import React from "react";
 import AccountNavbar from "../AccountNavbar";
 import { COLORS } from "@/utils/constants/colors";
 import Link from "next/link";
-import { SubmitErrorHandler, SubmitHandler, useForm } from "react-hook-form";
+import {
+    SubmitErrorHandler,
+    SubmitHandler,
+    useForm,
+    FieldError,
+} from "react-hook-form";
 import { TextInputField } from "@/components/shared/forms";
 import { EMAIL_REGEX } from "@/utils/constants/regex";
 import { Button } from "@/components/shared/elements";
 import { inter } from "@/utils/constants/fonts";
 import { Back } from "@/components/shared/svg/icons";
+import { FC } from "react";
+import { ClipLoader } from "react-spinners";
 
 interface FormFields {
-  email: string;
+    email: string;
 }
 
-const ForgetPassword: React.FC = () => {
-  const { handleSubmit, register } = useForm<FormFields>();
+interface Props {
+    onSubmit: SubmitHandler<FormFields>;
+    onError: SubmitErrorHandler<FieldError>;
+    isLoading: boolean;
+}
 
-  const onSubmit: SubmitHandler<FormFields> = (data) => {
-    console.log(data);
-  };
+const ForgetPassword: FC<Props> = ({ onSubmit, onError, isLoading }) => {
+    const { handleSubmit, register } = useForm<FormFields>();
 
-  const onError: SubmitErrorHandler<FormFields> = (error) => {
-    console.log(error);
-  };
+    return (
+        <>
+            <div className="flex flex-col h-screen">
+                <AccountNavbar />
+                <div className="flex justify-center items-center flex-grow">
+                    {/*grid place-items-center */}
+                    <div
+                        className={`flex flex-col px-5 py-8 gap-6 ${inter.className} md:py-[5%] max-w-xl w-full`}
+                    >
+                        <div className="flex flex-col gap-2">
+                            <Link href="/accounts/login">
+                                <Back
+                                    dimensions={{ width: 32 }}
+                                    color={COLORS.primary[700]}
+                                />
+                            </Link>
+                            <h1 className="text-3xl md:text-4xl font-semibold">
+                                Forgot Password
+                            </h1>
+                            <p className="text-sm md:text-base font-medium">
+                                Enter the email you used when you joined and
+                                we&apos;ll{" "}
+                                <span className="bg-tertiary">
+                                    send you a link to reset
+                                </span>{" "}
+                                your password.
+                            </p>
+                        </div>
 
-  return (
-    <>
-      <AccountNavbar />
-      <div className="grid place-items-center">
-        <div
-          className={`flex flex-col px-5 py-8 gap-6 ${inter.className} md:py-[5%] max-w-xl w-full`}
-        >
-          <div className="flex flex-col gap-2">
-            <Link href="/accounts/login">
-              <Back dimensions={{ width: 32 }} color={COLORS.primary[700]} />
-            </Link>
-            <h1 className="text-3xl md:text-4xl font-semibold">
-              Forgot Password
-            </h1>
-            <p className="text-sm md:text-base font-medium">
-              Enter the email you used when you joined and we&apos;ll{" "}
-              <span className="bg-tertiary">send you a link to reset</span> your
-              password.
-            </p>
-          </div>
+                        <form
+                            className="flex flex-col gap-5"
+                            onSubmit={handleSubmit(onSubmit, onError)}
+                        >
+                            <TextInputField
+                                field="email"
+                                label="Email"
+                                register={register}
+                                registerOptions={{
+                                    pattern: {
+                                        value: EMAIL_REGEX,
+                                        message:
+                                            "The email you entered is invalid",
+                                    },
+                                }}
+                                placeholder="Email"
+                            />
+                            <Button
+                                disabled={isLoading}
+                                className="bg-primary-700 flex justify-center items-center p-3 rounded-lg"
+                            >
+                                <ClipLoader
+                                    // className="py-3"
+                                    color="#BAF67E"
+                                    loading={isLoading}
+                                    // cssOverride={{
+                                    //     marginRight: "10px",
+                                    // }}
+                                    size={20}
+                                    aria-label="Loading Spinner"
+                                    data-testid="loader"
+                                />
 
-          <form
-            className="flex flex-col gap-5"
-            onSubmit={handleSubmit(onSubmit, onError)}
-          >
-            <TextInputField
-              field="email"
-              label="Email"
-              register={register}
-              registerOptions={{
-                pattern: {
-                  value: EMAIL_REGEX,
-                  message: "The email you entered is invalid",
-                },
-              }}
-              placeholder="Email"
-            />
-            <Button className="bg-primary-700 flex justify-center items-center p-3 rounded-lg">
-              <p className="text-primary-200 font-semibold text-sm">Send</p>
-            </Button>
-          </form>
-        </div>
-      </div>
-    </>
-  );
+                                <p
+                                    className={`text-primary-200 font-semibold text-sm ${
+                                        isLoading ? "hidden" : "block"
+                                    }`}
+                                >
+                                    Send
+                                </p>
+                            </Button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default ForgetPassword;
