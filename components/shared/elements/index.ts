5b25fc5dc20aa03d88b55fd53330e635d2c3d9e4
Rashad Aziz <rashad.aziz@ui.ac.id>
2023-10-03 17:52:13 +1000

    feat: added status pill shared component

--- components/shared/elements/index.ts
+++ components/shared/elements/index.ts
@@ -2,5 +2,6 @@ import Button from "./Button";
 import Card from "./Card";
 import Loader from "./Loader";
 import RoundedCircle from "./RoundedCircle";
+import StatusPill from "./StatusPill";
 
-export { Button, Loader, Card, RoundedCircle };
+export { Button, Loader, Card, RoundedCircle, StatusPill };
