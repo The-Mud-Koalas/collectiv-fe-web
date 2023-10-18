5a05e8a08687ac9f4d48326b269505ffb1ee6781
Harry-01 <harry@harry-mok.com>
2023-10-18 13:15:54 +1000

    fix: fix spacing and adjusted font sizes on mobile devices

--- components/features/home/LocationSubscription/Header.tsx
+++ components/features/home/LocationSubscription/Header.tsx
@@ -7,22 +7,22 @@ import { useAppContext } from "@/context/AppContext";
 interface HeaderProps {}
 
 const Header: FC<HeaderProps> = ({}) => {
-  const { user } = useAppContext();
-  return (
-    <div className={`${inter.className} max-w-4xl mx-auto mt-20`}>
-      <h1
-        className={`${inter.className} text-center text-primary-900 leading-snug text-4xl py-5 font-semibold md:leading-snug md:text-6xl lg:text-7xl`}
-      >
-        {user ? "Subscribe to our" : "Check out our available"}
-        <span className={`${garamond.className} italic font-light`}>
-          {" "}
-          Location
-        </span>{" "}
-        {user && "to get alerted"}
-      </h1>
-      {user && <TrackLocationToggle />}
-    </div>
-  );
+    const { user } = useAppContext();
+    return (
+        <div className={`${inter.className} max-w-4xl mx-auto mt-20`}>
+            <h1
+                className={`${inter.className} text-center text-primary-900 leading-snug text-5xl py-5 font-semibold md:leading-snug md:text-6xl lg:text-7xl`}
+            >
+                {user ? "Subscribe to our" : "Check out our available"}
+                <span className={`${garamond.className} italic font-light`}>
+                    {" "}
+                    Location
+                </span>{" "}
+                {user && "to get alerted"}
+            </h1>
+            {user && <TrackLocationToggle />}
+        </div>
+    );
 };
 
 export default Header;
